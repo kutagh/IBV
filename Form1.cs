5d95e48d69f7108df20c376dd4a69682c3704353
kutagh <mail@kutagh.com>
2014-01-15 21:00:31 +0100

    Cleaned up a bit and did a bit more work

--- Form1.cs
+++ Form1.cs
@@ -1,28 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.IO;
 
-namespace INFOIBV
-{
-    public partial class INFOIBV : Form
-    {
+namespace INFOIBV {
+    public partial class INFOIBV : Form {
         private Bitmap InputImage;
         private Bitmap OutputImage;
 
-        public INFOIBV()
-        {
+        public INFOIBV() {
             InitializeComponent();
         }
 
-        private void LoadImageButton_Click(object sender, EventArgs e)
-        {
-           if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
+        private void LoadImageButton_Click(object sender, EventArgs e) {
+            if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
             {
                 string file = openImageDialog.FileName;                     // Get the file name
                 imageFileName.Text = file;                                  // Show file name
@@ -36,11 +26,9 @@ namespace INFOIBV
             }
         }
 
-        private void applyButton_Click(object sender, EventArgs e)
-        {
+        private void applyButton_Click(object sender, EventArgs e) {
             if (InputImage == null) return;                                 // Get out if no input image
             if (OutputImage != null) OutputImage.Dispose();                 // Reset output image
-            OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // Create new output image
             Color[,] Image = new Color[InputImage.Size.Width, InputImage.Size.Height]; // Create array to speed-up operations (Bitmap functions are very slow)
 
             // Setup progress bar
@@ -51,10 +39,8 @@ namespace INFOIBV
             progressBar.Step = 1;
 
             // Copy input Bitmap to array            
-            for (int x = 0; x < InputImage.Size.Width; x++)
-            {
-                for (int y = 0; y < InputImage.Size.Height; y++)
-                {
+            for (int x = 0; x < InputImage.Size.Width; x++) {
+                for (int y = 0; y < InputImage.Size.Height; y++) {
                     Image[x, y] = InputImage.GetPixel(x, y);                // Set pixel color in array at (x,y)
                 }
             }
@@ -62,11 +48,9 @@ namespace INFOIBV
             //==========================================================================================
             // Start of own code
             //==========================================================================================
-            
-            for (int x = 0; x < InputImage.Size.Width; x++)
-            {
-                for (int y = 0; y < InputImage.Size.Height; y++)
-                {
+
+            for (int x = 0; x < InputImage.Size.Width; x++) {
+                for (int y = 0; y < InputImage.Size.Height; y++) {
                     Color pixelColor = Image[x, y];                         // Get the pixel color at coordinate (x,y)
                     var grayColor = (int) (pixelColor.R * 0.299 + pixelColor.G * 0.587 + pixelColor.B * 0.114);
                     Image[x, y] = Color.FromArgb(grayColor, grayColor, grayColor);                             // Set the new pixel color at coordinate (x,y)
@@ -75,92 +59,28 @@ namespace INFOIBV
             }
             Image = Image.Threshold(200, 0, 255);
 
+
             //==========================================================================================
             // End of own code
             //==========================================================================================
 
             // Copy array to output Bitmap
-            for (int x = 0; x < InputImage.Size.Width; x++)
-            {
-                for (int y = 0; y < InputImage.Size.Height; y++)
-                {
+            OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // Create new output image
+            for (int x = 0; x < InputImage.Size.Width; x++) {
+                for (int y = 0; y < InputImage.Size.Height; y++) {
                     OutputImage.SetPixel(x, y, Image[x, y]);               // Set the pixel color at coordinate (x,y)
                 }
             }
-            
+
             pictureBox2.Image = OutputImage;                         // Display output image
             progressBar.Visible = false;                                    // Hide progress bar
         }
-        
-        private void saveButton_Click(object sender, EventArgs e)
-        {
+
+        private void saveButton_Click(object sender, EventArgs e) {
             if (OutputImage == null) return;                                // Get out if no output image
             if (saveImageDialog.ShowDialog() == DialogResult.OK)
                 OutputImage.Save(saveImageDialog.FileName);                 // Save the output image
         }
 
-    }
-
-    public static class Operators {
-
-        /// <summary>
-        /// A threshold operation on a Color 2D array. Requires the colors to be in gray scale values, so that R, G and B have identical values.
-        /// </summary>
-        /// <param name="image">The Color 2D array to threshold</param>
-        /// <param name="threshold">Threshold minimum value, exclusive</param>
-        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
-        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
-        /// <returns>A thresholded image</returns>
-        public static Color[,] Threshold(this Color[,] image, int threshold, int trueValue = 1, int falseValue = 0) {
-            return thresholdFunc(image, trueValue, falseValue, v => v > threshold);
-        }
-
-        /// <summary>
-        /// A threshold operation on a Color 2D array. Requires the colors to be in gray scale values, so that R, G and B have identical values.
-        /// </summary>
-        /// <param name="image">The Color 2D array to threshold</param>
-        /// <param name="minThreshold">Threshold minimum value, exclusive</param>
-        /// <param name="maxThreshold">Threshold maximum value, exclusive</param>
-        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
-        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
-        /// <returns>A thresholded image</returns>
-        public static Color[,] ThresholdRange(this Color[,] image, int minThreshold, int maxThreshold, int trueValue = 1, int falseValue = 0) {
-            return thresholdFunc(image, trueValue, falseValue, v => v > minThreshold && v < maxThreshold);
-        }
-
-        /// <summary>
-        /// A threshold operation on a Color 2D array. Requires the colors to be in gray scale values, so that R, G and B have identical values. This is the generic threshold function that can be specialized to a specific function.
-        /// </summary>
-        /// <param name="image">The Color 2D array to threshold</param>
-        /// <param name="threshold">Threshold minimum value, exclusive</param>
-        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
-        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
-        /// <param name="predicate">Predicate for a pixel value that determines whether the pixel satisfies the threshold or not</param>
-        /// <returns>A thresholded image</returns>
-        private static Color[,] thresholdFunc(Color[,] image, int trueValue, int falseValue, Predicate<int> predicate) {
-            return colorThresholdFunc(image, trueValue, falseValue, color => predicate(color.R));
-        }
-
-        /// <summary>
-        /// A threshold operation on a Color 2D array. This is the generic threshold function that can be specialized to a specific function.
-        /// </summary>
-        /// <param name="image">The Color 2D array to threshold</param>
-        /// <param name="threshold">Threshold minimum value, exclusive</param>
-        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
-        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
-        /// <param name="predicate">Predicate for a pixel value that determines whether the pixel satisfies the threshold or not</param>
-        /// <returns>A thresholded image</returns>
-        private static Color[,] colorThresholdFunc(Color[,] image, int trueValue, int falseValue, Predicate<Color> predicate) {
-            var minResultColor = Color.FromArgb(falseValue, falseValue, falseValue);
-            var maxResultColor = Color.FromArgb(trueValue, trueValue, trueValue);
-            var result = new Color[image.GetLength(0), image.GetLength(1)];
-            for (int x = 0; x < image.GetLength(0); x++)
-                for (int y = 0; y < image.GetLength(1); y++)
-                    if (predicate(image[x, y]))
-                        result[x, y] = maxResultColor;
-                    else
-                        result[x, y] = minResultColor;
-            return result;
-        }
     }
 }
---
+++ Operators.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Drawing;
+
+namespace INFOIBV {
+    public static class Operators {
+
+        /// <summary>
+        /// A threshold operation on a Color 2D array. Requires the colors to be in gray scale values, so that R, G and B have identical values.
+        /// </summary>
+        /// <param name="image">The Color 2D array to threshold</param>
+        /// <param name="threshold">Threshold minimum value, exclusive</param>
+        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
+        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
+        /// <returns>A thresholded image</returns>
+        public static Color[,] Threshold(this Color[,] image, int threshold, int trueValue = 1, int falseValue = 0) {
+            return ThresholdFunc(image, trueValue, falseValue, v => v > threshold);
+        }
+
+        /// <summary>
+        /// A threshold operation on a Color 2D array. Requires the colors to be in gray scale values, so that R, G and B have identical values.
+        /// </summary>
+        /// <param name="image">The Color 2D array to threshold</param>
+        /// <param name="minThreshold">Threshold minimum value, exclusive</param>
+        /// <param name="maxThreshold">Threshold maximum value, exclusive</param>
+        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
+        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
+        /// <returns>A thresholded image</returns>
+        public static Color[,] ThresholdRange(this Color[,] image, int minThreshold, int maxThreshold, int trueValue = 1, int falseValue = 0) {
+            return ThresholdFunc(image, trueValue, falseValue, v => v > minThreshold && v < maxThreshold);
+        }
+
+        /// <summary>
+        /// A threshold operation on a Color 2D array. Requires the colors to be in gray scale values, so that R, G and B have identical values. This is the generic threshold function that can be specialized to a specific function.
+        /// </summary>
+        /// <param name="image">The Color 2D array to threshold</param>
+        /// <param name="threshold">Threshold minimum value, exclusive</param>
+        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
+        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
+        /// <param name="predicate">Predicate for a pixel value that determines whether the pixel satisfies the threshold or not</param>
+        /// <returns>A thresholded image</returns>
+        public static Color[,] ThresholdFunc(Color[,] image, int trueValue, int falseValue, Predicate<int> predicate) {
+            return ColorThresholdFunc(image, trueValue, falseValue, color => predicate(color.R));
+        }
+
+        /// <summary>
+        /// A threshold operation on a Color 2D array. This is the generic threshold function that can be specialized to a specific function.
+        /// </summary>
+        /// <param name="image">The Color 2D array to threshold</param>
+        /// <param name="threshold">Threshold minimum value, exclusive</param>
+        /// <param name="trueValue">The value that has to be assigned when the pixel value exceeds the threshold value</param>
+        /// <param name="falseValue">The value that has to be assigned when the pixel value does not exceed the threshold value</param>
+        /// <param name="predicate">Predicate for a pixel value that determines whether the pixel satisfies the threshold or not</param>
+        /// <returns>A thresholded image</returns>
+        public static Color[,] ColorThresholdFunc(Color[,] image, int trueValue, int falseValue, Predicate<Color> predicate) {
+            var minResultColor = Color.FromArgb(falseValue, falseValue, falseValue);
+            var maxResultColor = Color.FromArgb(trueValue, trueValue, trueValue);
+            var result = new Color[image.GetLength(0), image.GetLength(1)];
+            for (int x = 0; x < image.GetLength(0); x++)
+                for (int y = 0; y < image.GetLength(1); y++)
+                    result[x, y] = predicate(image[x, y]) ? maxResultColor : minResultColor;
+            return result;
+        }
+
+        /// <summary>
+        /// Generic operator function that combines two images into a new image.
+        /// </summary>
+        /// <param name="first">Color 2D array of the first image</param>
+        /// <param name="second">Color 2D array of the second image</param>
+        /// <param name="op">Operator function that takes a color from both images and produces a result color</param>
+        /// <returns>Color 2D array that represents the image after operator has been applied</returns>
+        public static Color[,] Op(Color[,] first, Color[,] second, Func<Color, Color, Color> op) {
+            if (first.GetLength(0) != second.GetLength(0) || first.GetLength(1) != second.GetLength(1))
+                throw new ArgumentException("Dimensions of the two images do not match");
+            var result = new Color[first.GetLength(0), first.GetLength(1)];
+            for (int x = 0; x < first.GetLength(0); x++)
+                for (int y = 0; y < first.GetLength(1); y++)
+                    result[x, y] = op(first[x, y], second[x, y]);
+            return result;
+        }
+    }
+}
